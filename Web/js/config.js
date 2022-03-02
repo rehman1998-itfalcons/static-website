f81c9bd81bf96f282d0c136a9a83127e1d58ea4a
unknown <abdullrehman1998@gmail.com>
2022-03-02 15:41:22 +0500

    changes

--- Web/js/config.js
+++ Web/js/config.js
@@ -2,7 +2,7 @@
 define(['text!../config/config_build.json'],
 function(build) {
     var config = {
-        dev: { host: "localhost", port: 8000, dispatcher: false },
+        dev: { host: "0.0.0.0", port: 8080, dispatcher: false },
         build: JSON.parse(build)
     };
     
