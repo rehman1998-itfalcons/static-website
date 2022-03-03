aa6a0ec47d379e6cf786660d22a915f8b37ef9a1
unknown <abdullrehman1998@gmail.com>
2022-03-03 22:58:14 +0500

    changes

--- start_worker.php
+++ start_worker.php
@@ -20,7 +20,7 @@ use \Server\WorldServer;
 require_once __DIR__ . '/vendor/autoload.php';
 
 // BrowserQuest Server
-$ws_worker = new Worker('Websocket://0.0.0.0:8080');
+$ws_worker = new Worker('Websocket://0.0.0.0:8000');
 $ws_worker->name = 'BrowserQuestWorker';
 $ws_worker->onWorkerStart = function($ws_worker)
 {
